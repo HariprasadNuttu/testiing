e1ade29e3fb423a7aaeee664694ef080e146d133
HariprasadNuttu <hariprasadnuttu@gmail.com>
2018-11-14 14:11:32 +0530

    Hariprasad-completed assesment

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,3 @@
-var con = require('./lib/db_setup');
-// var connectionObj = con.getConfig;
-// var createDb = con.createDb();
-
 var pg = require('pg');
 var conString = "postgres://test:test@localhost:5432/sammy";
 
@@ -12,23 +8,3 @@ var query = client.query('CREATE TABLE message_list(message_id uuid,message_cont
   console.log(err ? err.stack : res.rows[0]) // Hello World!
   client.end()
 });
-
-// var query = client.query("INSERT INTO  message_list(message_id,message_contents,received_ts) values ('943aa917-e7d4-11e8-855f-64006a553921','asdfasdfasdfas',DEFAULT)", (err, res) => {
-//   console.log(res);
-//   console.log(err ? err.stack : res.rows[0]) // Hello World!
-//   client.end()
-// });
-
-
-// query.on('end',function () {
-//   client.end();
-// })
-// createDb.then(function(result) {
-//         connectionObj.database = "Vdart_Technologies"
-//
-//         var createTable = con.createTable();
-//
-//     }, function(err) {
-//       console.log("FAilure");
-//         console.log(err);
-//     })
